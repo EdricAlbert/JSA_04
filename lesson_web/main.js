b9a4e991697a573019c365289ea90b4b84331d9c
Phạm Hồ Hữu Trí <oopscreeper2006@gmail.com>
2021-10-30 18:17:38 +0700

    d

---
+++ lesson_web/main.js
@@ -0,0 +1,21 @@
+function webFunction(){
+    var product = document.getElementsByClassName("allthings");
+    product = document.createElement("div");
+    product.classList.add("product");
+
+    var insideproduct = document.getElementsByClassName("product");
+    insideproduct = document.createElement("div");
+    product.classList.add("insideproduct");
+
+    var img = document.getElementsByClassName("insideproduct");
+    img = document.createElement("article");
+    img.classList.add("child");
+    
+    var img = document.getElementsByClassName("insideproduct");
+    img = document.createElement("article");
+    product.classList.add("child");
+   
+    var img = document.getElementsByClassName("insideproduct");
+    img = document.createElement("div");
+    product.classList.add("child");
+}
